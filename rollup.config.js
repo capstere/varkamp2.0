4e484d7db100dfea422e228203bc5e47c4c38807
capstere <jesper.fredriksson2@gmail.com>
2025-05-08 15:52:04 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import typescript from '@rollup/plugin-typescript';
 export default {
   input: 'src/index.ts',
   output: {
-    file: 'public/bundle.js',
+    file: 'docs/bundle.js',
     format: 'iife',
     sourcemap: true
   },
